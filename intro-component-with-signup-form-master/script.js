592b68999e348f934ca66c6b09b61711ca75e56d
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-06-16 10:34:18 -0600

    Update script.js

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,6 +1,6 @@
 var fields = document.querySelectorAll("input");
 var submitBtn = document.getElementById("submit");
-submitBtn.addEventListener("submit",function(){
+submitBtn.addEventListener("click",function(){
     for(let i=0; i< fields.length-1; i++){
         if(fields[i].value==""){
             fields[i].style.border="solid 2px var(--red)";
