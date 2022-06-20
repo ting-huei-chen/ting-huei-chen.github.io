1046e90233bdf4b60235f8fc125a25e48f41e5a9
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-06-20 16:58:21 -0600

    2022-06-20 update
    - fix slider progress bar
    -responsive design

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -48,3 +48,12 @@ togg.addEventListener("change",function(){
         console.log("else unchecked");
     }
 })
+if(window.innerWidth<600){
+    document.querySelector('.tag').innerHTML="-25%";
+}
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+    e.style.setProperty('--value', e.value);
+    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+  }
