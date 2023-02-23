79df94cb99b5b967aef85fe9c6c1c3f0b4d0a18f
Tîng-huē Tân <asakakei@gmail.com>
2023-02-23 00:30:31 -0700

    card component type A

---
+++ independent-study-project-1/card.js
@@ -0,0 +1,20 @@
+const createCard = () => {
+    console.log("card created!");
+}
+
+function dragstart_handler(ev) {
+    // Add the target element's id to the data transfer object
+    ev.dataTransfer.setData("application/my-app", ev.target.id);
+    ev.dataTransfer.effectAllowed = "move";
+  }
+  function dragover_handler(ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  }
+  function drop_handler(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    const data = ev.dataTransfer.getData("application/my-app");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  
